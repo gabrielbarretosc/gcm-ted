2edac4828797f98abe4beca6c98c64eb9a7be4e5
Bianca Magalhães <bianca@12min.com>
2020-12-02 22:57:55 -0300

    new empty test folder

---
+++ sigeh/src/test/java/br/com/sigeh/test/BOIntegrationTest.java
@@ -0,0 +1,15 @@
+package br.com.sigeh.test;
+
+import org.junit.jupiter.api.Test;
+
+public class BOIntegrationTest {
+
+	/**
+	 * Método a ser testado futuramente!
+	 * 
+	 * 
+	 */
+	@Test
+	public void criarNovaConsulta() {
+	}
+}
---
+++ sigeh/src/test/java/br/com/sigeh/test/BOTest.java
@@ -0,0 +1,15 @@
+package br.com.sigeh.test;
+
+import org.junit.jupiter.api.Test;
+
+public class BOTest {
+
+	/**
+	 * Método a ser testado futuramente!
+	 * 
+	 * 
+	 */
+	@Test
+	public void criarNovoProcedimento() {
+	}
+}
